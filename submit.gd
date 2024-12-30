64a87f8e3bc794eaa314040aecefe56baae55091
ghaisanghaisan <110543042+ghaisanghaisan@users.noreply.github.com>
2024-12-31 02:14:18 +0700

    cleanup junk files

--- submit.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var url = "http://localhost:8080/Aspirasi/post"
-
-func submit_form() -> void:
-	var body = JSON.stringify(SerasiForm.form_data)
-	var headers = ["Content-Type: application/json"]
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, body)
