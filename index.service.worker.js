c806ff56b68e0d66a85391a71d75ab934b273fdc
ghaisanghaisan <110543042+ghaisanghaisan@users.noreply.github.com>
2025-01-23 06:16:07 +0700

    performance issues

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737502868|39830988';
+const CACHE_VERSION = '1737572283|387200965';
 /** @type {string} */
 const CACHE_PREFIX = 'SERASI II-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
